1b147d0e5087ccf7283071a1ebcc69ef4a2e6be6
akhil babu <akhilbabu92@gmail.com>
2018-07-29 12:17:07 +0530

    fisrt coommit

--- spring5Webapp/src/main/java/guru/springframework/spring5Webapp/Spring5WebappApplication.java
+++ spring5Webapp/src/main/java/guru/springframework/spring5Webapp/Spring5WebappApplication.java
@@ -3,10 +3,27 @@ package guru.springframework.spring5Webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/*@SpringBootApplication*/
 public class Spring5WebappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5WebappApplication.class, args);
+		
+		
+		Object ob1=new Object();
+		Object ob2=new Object();
+		ob1=ob2;
+		System.out.println(ob2.equals(ob1
+				));
+		
+		//System.out.println(k);
+		/*SpringApplication.run(Spring5WebappApplication.class, args);*/
 	}
+	public static void test(int k)
+	{
+		
+		k=1;
+		
+		
+	}
+			
 }
